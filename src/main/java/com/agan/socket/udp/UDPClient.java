da4855dbbcd655ff5297a4b1e871813ab92ae9c7
bo <bobo0218bo@126.com>
2019-02-26 00:47:55 +0800

    udp

--- src/main/java/com/agan/socket/udp/UDPClient.java
+++ src/main/java/com/agan/socket/udp/UDPClient.java
@@ -6,29 +6,26 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 public class UDPClient {
-
-
     public static void main(String[] args) {
-        byte[] data = "hello world".getBytes();
         DatagramSocket datagramSocket = null;
         try {
             // 第一步，创建 DatagramSocket 实例，由于 UDP 是无连接的，不需要指定服务器的 IP 地址和端口号。
             datagramSocket = new DatagramSocket();
 
-            // 第二步，创建用于发送和接收的数据包。可以注意到，用于发送和接收的 DatagramPacket 实例的创建方式是不同的。
-            DatagramPacket sendPacket = new DatagramPacket(data, data.length, InetAddress.getLocalHost(), 8889);
+            // 第二步，创建DatagramPacket，用于发送的数据包。注意，数据包需要指定服务端的IP和端口。
+            byte[] request = "hello".getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(request, request.length, InetAddress.getLocalHost(), 9090);
 
-            // 第三步，使用 DatagramSocket 实例的 send(DatagramPacket p) 发送数据包。
+            // 第三步，使用DatagramSocket 向服务器，发送数据包。
             datagramSocket.send(sendPacket);
 
-            // 第四步，使用 DatagramSocket 实例 receive(DatagramPacket p) 来接收服务器返回的数据包。
-            DatagramPacket receivePacket = new DatagramPacket(new byte[data.length], data.length);
+            // 第四步，使用 DatagramSocket 接收服务器返回的数据包。
+            DatagramPacket receivePacket = new DatagramPacket(new byte[256], 256);
             datagramSocket.receive(receivePacket);
             System.out.println("Server said: " + new String(receivePacket.getData()));
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            // 6. close datagram socket
             if (datagramSocket != null) {
                 datagramSocket.close();
             }
